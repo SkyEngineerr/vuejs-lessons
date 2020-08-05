1d6f6f005d447b0e9117fab32e6d59ce321a1339
Eren Yilmaz <borulceprenss@gmail.com>
2020-08-05 12:26:21 +0200

    routes children

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,9 +2,9 @@
 <div>
     <h1>The User Page</h1>
     <hr>
-    <p>Loaded ID: {{ id}}</p>
     <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
-    
+    <hr>
+    <router-view></router-view>
 </div>
 
 </template>
@@ -13,17 +13,12 @@
 export default {
     data(){
         return{
-            id: this.$route.params.id
-        }
-    },
-    watch:{
-        '$route'(to, from){
-            this.id = to.params.id
+            
         }
     },
     methods:{
         navigateToHome(){
-            this.$router.push('/');
+            this.$router.push({name:'home'})
         }
     }
 }

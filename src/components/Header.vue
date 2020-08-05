44bfeb2bf595cd742ae40758e73ab0ea4af41d57
Eren Yilmaz <borulceprenss@gmail.com>
2020-08-05 11:26:28 +0200

    routes

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+        <ul class="nav nav-tabs">
+            <router-link to="/" tag="li" activate-class="activate" exact> <a>Home</a> </router-link>
+            <router-link to="/user/1" tag="li" activate-class="activate"> <a>User 1</a> </router-link>
+            <router-link to="/user/2" tag="li" activate-class="activate"> <a>User 2</a> </router-link>
+
+    
+    </ul>
+</template>   

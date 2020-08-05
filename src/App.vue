80e48783203f625c7c10088e5555b8504b1f0f3e
Eren Yilmaz <borulceprenss@gmail.com>
2020-08-05 13:03:24 +0200

    routers

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
                 <hr>
                 
                 <router-view name="header-top"></router-view>
-                <transition name="slide">
-                     <router-view>Eren</router-view>
+                <transition name="slide" mode="out-in">
+                     <router-view></router-view>
                 </transition>
                 <router-view name="header-bottom"></router-view>
                 
@@ -29,5 +29,53 @@ export default {
 </script>
 
 <style>
+  /* .fade-enter{
+     opacity: 0;
+ }
 
+ .fade-enter-active{
+     transition: opacity 1s;
+ }
+
+ .fade-leave{
+
+ } */
+
+ .fade-leave-active{
+     transition: opacity 1s;
+     opacity: 0;
+ }
+
+ .slide-enter{
+     opacity: 0;
+     /* transform: translateY(20px); */
+ }
+ .slide-enter-active{
+     animation: slide-in 1s ease-out forwards;
+     transition: opacity .5s;;
+ }
+ .slide-leave{
+     
+ }
+ .slide-leave-active{
+    animation: slide-out 1s ease-in-out forwards;
+    transition: opacity 3s;
+    opacity: 0;
+ }
+ @keyframes slide-in{
+     from{
+         transform: translateY(20px);
+     }
+     to{
+         transform: translateY(0);
+     }
+ }
+ @keyframes slide-out{
+     from{
+         transform: translateY(0);
+     }
+     to{
+         transform: translateY(20px);
+     }
+ }
 </style>
